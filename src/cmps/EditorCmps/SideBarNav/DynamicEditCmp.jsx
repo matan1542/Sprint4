19d221ad26936bf512787310761eaec815aa1e90
Nadav Nassi <nadav.nassi@gmail.com>
2021-05-31 18:04:17 +0300

    commit

--- src/cmps/EditorCmps/SideBarNav/DynamicEditCmp.jsx
+++ src/cmps/EditorCmps/SideBarNav/DynamicEditCmp.jsx
@@ -1,25 +1,28 @@
-import {EditFontSize} from './EditorControler/EditFontSize'
-import {EditLetterSpacing} from './EditorControler/EditLetterSpacing'
-import {EditFontFamily} from './EditorControler/EditFontFamily'
-import {EditColor} from './EditorControler/EditColor'
-import {EditFlexDirection} from './EditorControler/EditFlexDirection'
+import { EditFontSize } from './EditorControler/EditFontSize'
+import { EditLetterSpacing } from './EditorControler/EditLetterSpacing'
+import { EditFontFamily } from './EditorControler/EditFontFamily'
+import { EditColor } from './EditorControler/EditColor'
+import { EditFlexDirection } from './EditorControler/EditFlexDirection'
+import { EditBackgroundImg } from './EditorControler/EditBackgroundImg'
 
-export  function DynamicEditCmp({attribute, value, currCmp, onUpdateCurrCmp}) {
+export function DynamicEditCmp({ attribute, value, currCmp, onUpdateCurrCmp }) {
     console.log(attribute, value);
     switch (attribute) {
         case "fontSize":
-            return <EditFontSize val={value} onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp}/>
-        case "fontFamily": 
-            return <EditFontFamily val={value} onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp}/>
-        case "color": 
-            return <EditColor att={attribute} val={value} onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp}/>            
-        case "backgroundColor": 
-            return <EditColor att={attribute} val={value} onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp}/>            
-        case "letterSpacing": 
-            return <EditLetterSpacing val={value} onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp}/>
-        case "flexDirection": 
-            return <EditFlexDirection val={value} onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp}/>
-        default : return null    
+            return <EditFontSize val={value} onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp} />
+        case "fontFamily":
+            return <EditFontFamily val={value} onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp} />
+        case "color":
+            return <EditColor att={attribute} val={value} onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp} />
+        case "backgroundColor":
+            return <EditColor att={attribute} val={value} onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp} />
+        case "letterSpacing":
+            return <EditLetterSpacing val={value} onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp} />
+        case "flexDirection":
+            return <EditFlexDirection val={value} onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp} />
+        case "backgroundImage":
+            return <EditBackgroundImg onUpdateCurrCmp={onUpdateCurrCmp} currCmp={currCmp} />
+        default: return null
     }
 }
 
