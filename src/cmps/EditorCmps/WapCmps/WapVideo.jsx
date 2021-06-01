04735d3783fb9a99eb73cb146e23f91eed218616
matan1542 <matan8380@gmail.com>
2021-06-01 21:56:56 +0300

    video

--- src/cmps/EditorCmps/WapCmps/WapVideo.jsx
+++ src/cmps/EditorCmps/WapCmps/WapVideo.jsx
@@ -5,15 +5,15 @@ import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined'
 
 export function WapVideo({onCmpFocus,onDeleteCmp,cmp}) {
   return (
-    <div className="player-wrapper">
+    <div className="player-wrapper" style={{...cmp.info.style}}>
       <ReactPlayer
         className="react-player"
         url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
         width="100%"
         height="100%"
       />
-      <div className="wap-section-tool">
-        <button className="wap-el-btn-edit" onClick={() => onCmpFocus(cmp)}>
+      <div className="wap-section-tool" style={{right:"100px"}}>
+        <button className="wap-el-btn-edit" onClick={(ev) => onCmpFocus(ev,cmp)}>
           <EditOutlinedIcon />
         </button>
         <button className="wap-el-btn-del" onClick={() => onDeleteCmp(cmp.id)}>
