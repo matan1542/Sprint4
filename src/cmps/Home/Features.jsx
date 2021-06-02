7814b7f3fe3b1bd64fc73ff9858b8f09639f33f4
matan1542 <matan8380@gmail.com>
2021-06-02 14:28:10 +0300

    home

--- src/cmps/Home/Features.jsx
+++ src/cmps/Home/Features.jsx
@@ -1,12 +1,28 @@
-import { Component } from "react";
+import { useCallback, useRef } from 'react';
+import InView, { useInView } from 'react-intersection-observer';
 
-export class Features extends Component {
-  render() {
+export function Features() {
+  const ref = useRef();
+  const [inViewRef, inView] = useInView();
+
+  // Use `useCallback` so we don't recreate the function on each render - Could result in infinite loop
+  const setRefs = useCallback(
+    (node) => {
+      // Ref's from useRef needs to have the node assigned to `current`
+      ref.current = node;
+      // Callback refs, like the one from `useInView`, is a function that takes the node as an argument
+      inViewRef(node);
+    },
+    [inViewRef],
+  );
+
+  console.log(inView)
     return (
       <section className="dark-section" id="features">
+        {/* <InView as="div" onChange={(inView, entry) => console.log('Inview:', inView)}></InView> */}
         <div className="container">
           <h3 className="section-title">Meet our powerful Platform</h3>
-          <div className="feature-container flex">
+          <div ref={setRefs} className="feature-container flex">
             <div className="text-container">
               <h2>Build a website easily!</h2>
               <p>
@@ -18,7 +34,7 @@ export class Features extends Component {
             </div>
             <img src="images/feature4.png" alt="feature1" />
           </div>
-          <div className="feature-container flex">
+          <div ref={setRefs} className="feature-container flex">
             <img src="images/feature2.png" alt="feature2" />
             <div className="text-container">
               <h2>Suited for any of your needs</h2>
@@ -30,7 +46,7 @@ export class Features extends Component {
               </p>
             </div>
           </div>
-          <div className="feature-container flex">
+          <div ref={setRefs} className="feature-container flex">
             <div className="text-container">
               <h2>Just Drag &amp; Drop</h2>
               <p>
@@ -44,5 +60,4 @@ export class Features extends Component {
         </div>
       </section>
     );
-  }
 }
