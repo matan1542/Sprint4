d9f426d3dc803fcc7243527bd6b00974fae87663
matan1542 <matan8380@gmail.com>
2021-06-03 12:01:40 +0300

    gp

--- src/cmps/EditorCmps/WapCmps/WapNav.jsx
+++ src/cmps/EditorCmps/WapCmps/WapNav.jsx
@@ -7,7 +7,7 @@ import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined'
 export function WapNav({ onClickFunc, cmp, wap, onDeleteCmp, onCmpFocus, onUpdateCurrCmp, updateWap, isEdit }) {
   if (!isEdit) {
     return (
-      <nav className="wap-section flex w-75 publish"
+      <nav className="wap-section publish flex w-75"
         style={cmp.info.style}>
         {cmp.cmps && <EditorWapCmps cmp={cmp} wap={wap} isEdit={isEdit} updateWap={updateWap} onCmpFocus={onCmpFocus} onUpdateCurrCmp={onUpdateCurrCmp} onDeleteCmp={onDeleteCmp} />}
       </nav>
