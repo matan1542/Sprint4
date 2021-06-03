29ac696d9780b8734691dbf3653f0311c82a8a28
Nadav Nassi <nadav.nassi@gmail.com>
2021-06-03 21:46:41 +0300

    preview.jsx

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,13 @@ import { Home } from './pages/Home.jsx'
 import { Templates } from './pages/Template.jsx'
 import { Editor } from './pages/Editor.jsx'
 import { Publish } from './pages/Publish.jsx'
+import { Preview } from './pages/Preview.jsx'
 
 export const routes = [
+    {
+        path: '/preview/:wapId',
+        component: Publish
+    },
     {
         path: '/publish/:wapId',
         component: Publish
