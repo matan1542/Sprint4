6885c33e9c9e1d85c06491b769eb87e22d693b14
Nadav Nassi <nadav.nassi@gmail.com>
2021-06-06 19:07:41 +0300

    commit

--- src/pages/Template.jsx
+++ src/pages/Template.jsx
@@ -25,7 +25,7 @@ class _Templates extends Component {
   };
 
   render() {
-    const waps = this.props.waps.filter(wap => wap.isPublic === true)
+    // const waps = this.props.waps.filter(wap => wap.isPublic === true)
     if (!waps) return <Loader />;
     return (
       <section className="template-section">
