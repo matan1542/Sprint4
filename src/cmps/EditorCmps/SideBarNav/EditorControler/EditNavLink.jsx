a7daa705209f308d3170968693a84d5c7653a9dc
Nadav Nassi <nadav.nassi@gmail.com>
2021-06-06 10:15:19 +0300

    commit

---
+++ src/cmps/EditorCmps/SideBarNav/EditorControler/EditNavLink.jsx
@@ -0,0 +1,27 @@
+import { TextField } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+
+export function EditNavLink({ cmp, onUpdateCurrCmp }) {
+    const linkToEdit = {
+        link: cmp.info.action.link
+    }
+    const [urlState, setUrl] = useState(linkToEdit)
+
+    useEffect(() => {
+        onEditUrl()
+    }, [urlState])
+
+    const onEditUrl = () => {
+        const cmpToUpdate = { ...cmp }
+        cmpToUpdate.info.action.link = urlState.link
+        onUpdateCurrCmp(cmpToUpdate)
+    }
+
+    return (
+        <div className="edit-link">
+            <TextField label="add link"
+                value={urlState.link}
+                onChange={({ target }) => setUrl({ link: target.value })} />
+        </div>
+    )
+}
